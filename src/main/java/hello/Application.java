04d660f2313c4628556d4c238d940b6551a4f256
Ashish Sharda <asharda@tree.com>
2019-10-18 20:38:50 -0400

    Add all my files

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", "8090"));
+        app.run(args);
+    }
+}
